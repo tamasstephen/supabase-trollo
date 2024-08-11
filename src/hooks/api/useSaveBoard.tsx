030773cc81cf8e3f718f23efc832aab5b6515e89
tamasstephen <kicsitnagyon@gmail.com>
2024-08-11 10:48:28 +0200

    refactor: refactor saveBoard

--- src/hooks/api/useSaveBoard.tsx
+++ src/hooks/api/useSaveBoard.tsx
@@ -1,11 +1,43 @@
 import { useAuthContext } from "..";
 import { Board, BoardsFormElement } from "@/types";
+import { SupabaseClient } from "@supabase/supabase-js";
 import { useState } from "react";
+
+type Payload = Omit<Board, "image" | "id">;
 
 export const useSaveBoard = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const { supabaseClient } = useAuthContext();
+
+  const saveBoardBackGround = async (
+    boardCover: File,
+    supabaseClient: SupabaseClient,
+    payload: Payload
+  ) => {
+    const { data, error } = await supabaseClient.storage
+      .from("board_cover")
+      .upload(`board_cover/${boardCover.name}`, boardCover, {
+        cacheControl: "3600",
+        upsert: false,
+      });
+    if (error) {
+      setError(true);
+      return;
+    }
+    payload.background = data.path;
+  };
+
+  const saveBoardData = async (
+    payload: Payload,
+    supabaseClient: SupabaseClient
+  ) => {
+    const response = await supabaseClient.from("boards").insert(payload);
+    if (!response || response.error) {
+      setError(true);
+      return;
+    }
+  };
 
   const saveBoard = async (event: React.FormEvent<BoardsFormElement>) => {
     if (!supabaseClient) {
@@ -13,28 +45,14 @@ export const useSaveBoard = () => {
       return;
     }
     event.preventDefault();
-    const payload: Omit<Board, "image" | "id"> = { name: "" };
+    setLoading(true);
+    const payload: Payload = { name: "" };
     payload.name = event.currentTarget.elements.boardName.value;
     if (event.currentTarget.elements.boardCover.files?.length) {
-      setLoading(true);
       const boardCover = event.currentTarget.elements.boardCover.files[0];
-      const { data, error } = await supabaseClient.storage
-        .from("board_cover")
-        .upload(`board_cover/${boardCover.name}`, boardCover, {
-          cacheControl: "3600",
-          upsert: false,
-        });
-      if (error) {
-        setError(true);
-        return;
-      }
-      payload.background = data.path;
+      await saveBoardBackGround(boardCover, supabaseClient, payload);
     }
-    const response = await supabaseClient.from("boards").insert(payload);
-    if (!response || response.error) {
-      setError(true);
-      return;
-    }
+    await saveBoardData(payload, supabaseClient);
     setLoading(false);
   };
 
