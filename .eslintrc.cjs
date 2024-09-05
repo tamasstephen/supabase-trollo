7d9fbc9ca1d12c3423e1194dedf5e0c3e4d8ca1c
tamasstephen <kicsitnagyon@gmail.com>
2024-09-05 11:15:40 +0200

    chore: fix linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,11 @@
-import pluginQuery from "@tanstack/eslint-plugin-query";
-
 module.exports = {
-  ...pluginQuery.configs["flat/recommended"],
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
